e75cf5b0f80bb25521de731fd2e471704985e44d
Sam Farmer <sam_farmer@hotmail.com>
2016-08-29 10:37:19 +0100

    Basic devise setup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
